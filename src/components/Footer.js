931b634888ffd95a9f3ac1f0cf8695b1f06bfd8e
tanujap148 <tanujap148@gmail.com>
2018-10-26 15:38:17 +0530

    new changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,10 @@ import linkedin from '../images/linkedin-logo.png';
 import twitter from '../images/twitter-logo.png';
 import youtube from '../images/youtube-logo.png';
 import googleplay from '../images/GooglePlay.png';
+import appr from '../images/apprentices.jpg';
+import fw from '../images/freshersworld.jpg';
+import schoolguru from '../images/school_guru.jpg';
+import teamlease from '../images/teamlease-skills.jpg';
 import './Footer.css';
 
 class Footer extends Component {
@@ -72,7 +76,7 @@ class Footer extends Component {
       							</a>
       						</div>
       						<div className="contact margin_top_10">Easy Job access on the go</div>
-      						<div className="inline-block social-icon">
+      						<div className="inline-block social-icon gplay">
       							<a href="https://play.google.com/store/apps/details?id=com.teamlease.jobs&amp;referrer=utm_source%3Dfooter%26utm_medium%3Dweb%26utm_campaign%3Dweb" data-toggle="toggle" title="Try our App on Google Play Store">
       								<img src={googleplay} className="social_img gplay" alt="twitter" />
       							</a>
@@ -80,6 +84,62 @@ class Footer extends Component {
 			         </div>
 						</div>
 		     </div>
+
+         <div className="footer-last-section">
+ 						<div className="text-center">
+     						<div className="inline-block font-12 pad_bottom_20 pad_top_0">
+       							<div className="color-777576  margin-top-15">Copyright © 2018 TeamLease. All rights reserved</div>
+       							<div className="color-777576 margin_top_8">ISO 27001 Information Technology Security Management System. Certified by British Standards</div>
+       							<div className="font-12 fotter-bottom-links margin_bottom_0 margin_top_20">
+         								<a className="footer-hover color-777576" href="/terms-conditions" target="_blank">Terms &amp; Conditions</a>
+         								<span className="padding-right-10 padding-left-10 color-17284e">|</span>
+         								<a className="footer-hover color-777576 " href="/privacy-policy" target="_blank">Privacy Policy</a>
+         								<span className="padding-right-10 padding-left-10 color-17284e">|</span>
+         								<a className="footer-hover color-777576 " href="/sitemap" target="_blank">Sitemap</a>
+       							</div>
+     						</div>
+ 						</div>
+ 		    </div>
+
+        <div className="client_logos">
+            <div className="center-align text-center margin_top_20 margin_bottom_20">
+              <div className="inline-block color-777576 margin_right_20"><div className="site_text">Partner Sites</div></div>
+              <div className="inline-block companies-logo-footer">
+                  <a href="https://www.freshersworld.com" target="_blank">
+                      <img alt="freshersworld.com" title="Freshersworld" src={fw} />
+                  </a>
+              </div>
+              <div className="inline-block companies-logo-footer">
+                  <a href="http://www.teamleaseuniversity.ac.in" target="_blank">
+                      <img alt="teamleaseuniversity.ac.in" title="Teamlease Skills" src={teamlease} />
+                  </a>
+              </div>
+              <div className="inline-block companies-logo-footer">
+                  <a href="https://www.apprentices.in" target="_blank">
+                      <img alt="apprentices.in" title="Apprentices" src={appr} />
+                  </a>
+              </div>
+              <div className="inline-block companies-logo-footer">
+                  <a href="http://www.schoolguru.in" target="_blank">
+                      <img alt="schoolguru.in" title="Schoolguru" src={schoolguru} />
+                  </a>
+              </div>
+            </div>
+        </div>
+
+        <div className="mob_alternate">
+            <div className="nav-footer-corp margin_bottom_20">
+              <a href="https://tlconnect.teamlease.com/SecureLogin" class="text-decoration corp-text">
+                Associate Login - Login Now
+              </a>
+            </div>
+            <div className="nav-footer-corp">Corporate Website -
+							<a href="https://www.teamleasegroup.com" className="text-decoration corp-text">
+								TeamLeaseGroup.com
+							</a>
+						</div>
+            <div className="copyright-text font-12">Copyright © 2018 TeamLease. All rights reserved</div>
+        </div>
 
       </footer>
     );
